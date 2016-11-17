6d56f44cbbc0c5624f98ecd24e3ee108d7dbc639
Alexis Schad <schadoc_alex@hotmail.fr>
2016-11-17 01:23:22 +0100

    apples and oranges does not want to work online

--- challenges/Finals Spring 2015 - Apples and Oranges/main.js
+++ challenges/Finals Spring 2015 - Apples and Oranges/main.js
@@ -31,57 +31,71 @@ eval(fs.readFileSync('../../algebre.js').toString());
 getInput(["| i-A i-O i-M i-X i-Y i-Mf"], main);
 
 function main() {
-    let aMax = computeMax(A, O, M, X, Y, Mf);
-    let oMax = computeMax(O, A, M, Y, X, Mf);
-    console.log(aMax, oMax);
+    ///!!!!\ does not work /!!!!\\\
+    ///!!!!\ does not work /!!!!\\\
+    ///!!!!\ does not work /!!!!\\\
+    ///!!!!\ does not work /!!!!\\\
+    ///!!!!\ does not work /!!!!\\\
+    ///!!!!\ does not work /!!!!\\\
+    M += A * X;
+    M += O * Y;
+    M -= Mf;
+    let [d, x1, y1] = euclid(X, Y);
+    if(M < 0 || M % d !== 0) {
+        exit();
+    } else {
+        let solution = diophantineEquation(X/d, Y/d, M/d);
+        if(solution === null) {
+            exit();
+        } else {
+            let [x1, y1] = solution;
+            console.log((M - y1 * Y) / X, (M - x1 * X) / Y);
+        }
+    }
 }
 
-function computeMax(A, O, M, X, Y, Mf) {
-    function buyA(x = Infinity) {
-        let diff = Math.floor(Math.min(x, M) / X);
-        A += diff;
-        M -= diff * X;
+// a*x + b*y = c
+function diophantineEquation(a, b, c) {
+    // if(a * b - 0.5 * (a - 1) * (b - 1) < 1) {
+    //     exit();
+    // }
+
+    let q = Math.floor(c / (a * b));
+    let r = c % (a * b);
+
+    // a*x1 + b*y1 = d in Z
+    let [d, x1, y1] = euclid(a, b);
+    // console.log(a+"*"+x1+" + "+b+"*"+y1+" = "+d);
+    // if(r % d !== 0) {
+    //     exit();
+    // }
+    // a*x2 + b*y2 = r in Z
+    let x2 = x1 * r / d;
+    let y2 = y1 * r / d;
+    // console.log(a+"*"+x2+" + "+b+"*"+y2+" = "+r);
+
+    // a*x3 + b*y3 = r in N
+    // a*x4 + b*y4 = r in N
+    let tmp = (a * y2 - b * x2) / (a*a+b*b);
+    let k1 = Math.floor(tmp);
+    let k2 = Math.ceil(tmp);
+    let x3 = x2 + b * k1;
+    let y3 = y2 - a * k1;
+    let x4 = x2 + b * k2;
+    let y4 = y2 - a * k2;
+    // console.log(a+"*"+x3+" + "+b+"*"+y3+" = "+r);
+    // console.log(a+"*"+x4+" + "+b+"*"+y4+" = "+r);
+
+    if(x3 >= 0 && y3 >= 0 && Math.floor(x3) === x3 && Math.floor(y3) === y3) {
+        return [x3, y3];
     }
-    function buyO(y = Infinity) {
-        let diff = Math.floor(Math.min(y, M) / Y);
-        O += diff;
-        M -= diff * Y;
-    }
-    function sellA(x = Infinity) {
-        let diff = Math.min(Math.floor(x / X), A);
-        A -= diff;
-        M += diff * X;
-    }
-    function sellO(y = Infinity) {
-        let diff = Math.min(Math.floor(y / Y), O);
-        O -= diff;
-        M += diff * Y;
+    if(x4 >= 0 && y4 >= 0 && Math.floor(x4) === x4 && Math.floor(y4) === y4) {
+        return [x4, y4];
     }
 
-    sellA();
-    sellO();
-    M -= Mf;
-    if(M < 0) {
-        exit();
-    }
-
-    let [gcd,u,v] = euclid(X, Y);
-    let ppcm = X * Y / gcd;
-
-    let mod;
-    while((mod = M % X) !== 0 && O * Y < ppcm) {
-        buyO(mod);
-    }
-
-    buyA();
-
-    if(M > 0) {
-        exit();
-    }
-    return A;
+    return null;
 }
 
 function exit() {
-    console.log("Impossible");
-    process.exit();
+    console.log(" Impossible ");
 }
