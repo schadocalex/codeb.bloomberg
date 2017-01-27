947853ed9fecc070257f7fd908d91e27937320bf
Mewen <mewenmichel@yahoo.fr>
2017-01-27 12:36:35 +0100

    Added segment support

--- geometry.js
+++ geometry.js
@@ -3,13 +3,15 @@
  *
  * Line-line intersection
  *
- * @param line a1 = { x: x1, y: y1 }
- * @param line a2 = { x: x2, y: y2 }
- * @param line b1 = { x: x3, y: y3 }
- * @param line b2 = { x: x4, y: y4 }
- * @returns Intersection point [x, y] or null if the lines are parallel
+ * @param pt a1 = { x: x1, y: y1 }
+ * @param pt a2 = { x: x2, y: y2 }
+ * @param pt b1 = { x: x3, y: y3 }
+ * @param pt b2 = { x: x4, y: y4 }
+ * @param bool aIsSegment, optionnal, default : a is a line
+ * @param bool bIsSegment, optionnal, default : b is a line
+ * @returns Intersection point [x, y] or null if the lines doesn't intersect
  */
-function lineLineIntersection( { x: x1, y: y1 }, { x: x2, y: y2 }, { x: x3, y: y3 }, { x: x4, y: y4 } ) {
+function lineLineIntersection( { x: x1, y: y1 }, { x: x2, y: y2 }, { x: x3, y: y3 }, { x: x4, y: y4 }, aIsSegment = false, bIsSegment = false) {
     let denominator = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4);
 
     if (denominator == 0) {
@@ -21,6 +23,25 @@ function lineLineIntersection( { x: x1, y: y1 }, { x: x2, y: y2 }, { x: x3, y: y
 
     let py = (x1 * y2-y1 * x2) * (y3-y4) - (y1-y2) * (x3 * y4-y3 * x4);
     py /= denominator;
+
+    // px is in ([x1, x2] or [x1, x2]) and in ([x3, x4] or [x4, x3])
+    if (   (aIsSegment
+	    	&& (px < Math.min(x1, x2)
+		    	|| px > Math.max(x1, x2)
+		    	|| py < Math.min(y1, y2)
+		    	|| py > Math.max(y1, y2)
+		    	)
+	    	)
+    	|| (bIsSegment
+	    	&& ( px < Math.min(x3, x4)
+		    	|| px > Math.max(x3, x4)
+		    	|| py < Math.min(y3, y4)
+		    	|| py > Math.max(y3, y4)
+		    	)
+	    	)
+    	) {
+    	return null;
+    }
 
     return [px, py];
 }
@@ -43,3 +64,17 @@ var b2 = { x: 5, y: 4 };
 console.log(lineLineIntersection(a1, a2, b1, b2));
 
 */
+
+
+/**
+ * Segment-segment intersection
+ *
+ * @param line a1 = { x: x1, y: y1 }
+ * @param line a2 = { x: x2, y: y2 }
+ * @param line b1 = { x: x3, y: y3 }
+ * @param line b2 = { x: x4, y: y4 }
+ * @returns Intersection point [x, y] or null if the segments doesn't intersect
+ */
+function segmentSegmentIntersection( { x: x1, y: y1 }, { x: x2, y: y2 }, { x: x3, y: y3 }, { x: x4, y: y4 } ) {
+	return lineLineIntersection( { x: x1, y: y1 }, { x: x2, y: y2 }, { x: x3, y: y3 }, { x: x4, y: y4 }, true, true);   
+}
